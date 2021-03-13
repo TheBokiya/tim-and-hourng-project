2feababf37cd9efa8a50f9977a5ebf022e572aec
Heng Bodyka <heng.bodyka@sabay.com>
2021-03-13 10:17:55 +0700

    fixed event detailc component layout on mobile

--- src/pages/wedding.js
+++ src/pages/wedding.js
@@ -76,11 +76,8 @@ const EveningProgram = [
 const EventDetailComponent = (props) => {
   return (
     <div className="flex sm:flex-1 sm:flex-col my-3 justify-center">
-      <div className="sm:flex sm:justify-center">
-        <FontAwesomeIcon
-          icon={props.icon}
-          className="text-gold text-xl self-center mr-5 sm:mr-0 sm:mb-5"
-        />
+      <div className="self-center mr-5 sm:mr-0 sm:mb-5 sm:flex sm:justify-center">
+        <FontAwesomeIcon icon={props.icon} className="text-gold text-xl" />
       </div>
       <div className="flex flex-col">
         <h5 className="font-sans uppercase sm:text-center">{props.title}</h5>
