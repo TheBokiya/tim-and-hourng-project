2593a0fdedc677d0ac48c82b2de797be27f8d4b6
TheBokiya <hengbodyka@gmail.com>
2021-04-10 15:10:45 +0700

    removed code from layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Image from "../images/img-cover.png";
 import SEO from "./seo";
 import Header from "../components/header";
 
 const Layout = ({ children, data }) => {
-  const coverImage = data.edges.node.childImageSharp;
-  console.log(coverImage);
   return (
     <div className="flex flex-col justify-between">
       <SEO title="Tim & Hourng" />
