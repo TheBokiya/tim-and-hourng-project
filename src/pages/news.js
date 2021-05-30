3054278f5eb2c7d28482e965e514fcd9e9bb676f
TheBokiya <hengbodyka@gmail.com>
2021-05-30 17:23:54 +0700

    update news style

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,3 +1,5 @@
+import { Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -9,14 +11,14 @@ export const newsQuery = graphql`
         node {
           childMarkdownRemark {
             frontmatter {
-              date
+              date(formatString: "MMMM DD, YYYY")
               slug
               title
-              # featuredImage {
-              #   childImageSharp {
-              #     gatsbyImageData
-              #   }
-              # }
+              featuredImage {
+                childImageSharp {
+                  gatsbyImageData
+                }
+              }
             }
           }
         }
@@ -26,7 +28,6 @@ export const newsQuery = graphql`
 `;
 
 const NewsPage = ({ data }) => {
-  console.log(data);
   return (
     <Layout>
       <SEO title="News" />
@@ -34,9 +35,27 @@ const NewsPage = ({ data }) => {
         <h1 className="text-primary text-center uppercase tracking-wide text-navy font-sans text-6xl font-black my-10">
           News
         </h1>
-        <div className="flex felx-wrap">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-36">
           {data.allFile.edges.map(({ node }, index) => (
-            <div key={index}>{node.childMarkdownRemark.frontmatter.title}</div>
+            <Link
+              to={node.childMarkdownRemark.frontmatter.slug}
+              className="no-underline group"
+            >
+              <div key={index}>
+                <GatsbyImage
+                  image={getImage(
+                    node.childMarkdownRemark.frontmatter.featuredImage
+                  )}
+                  className="transition duration-300 ease-in-out opacity-80 group-hover:opacity-100"
+                />
+                <h3 className="font-sans uppercase transition duration-300 ease-in-out group-hover:text-gold">
+                  {node.childMarkdownRemark.frontmatter.title}
+                </h3>
+                <h5 className="text-gray-300">
+                  {node.childMarkdownRemark.frontmatter.date}
+                </h5>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
